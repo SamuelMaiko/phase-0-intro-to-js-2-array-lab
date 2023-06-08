90449f77003112819af9cd87870bda79f03c8800
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-09 01:36:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+cats.splice(cats.length,0,name);
+}
+
+function destructivelyPrependCat(name){
+cats.splice(0,0,name);
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.splice(cats.length-1,1);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1);
+}
+
+function appendCat(name){
+    const caty=[...cats,name];
+    return caty;
+}
+function prependCat(name){
+   const caty2=[name,...cats];
+   return caty2;
+}
+
+function removeLastCat(){
+    const caty3=cats.slice(0,-1);
+    return caty3;
+}
+function removeFirstCat() {
+    const caty4=[...cats];
+    return caty4.slice(1);
+}
